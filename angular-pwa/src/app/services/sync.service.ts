5f019ed91890c807238d4461007129df6adf816f
Jaromir Konupka <jkonupka@gmail.com>
2020-03-26 00:12:37 +0100

    sync interval

--- angular-pwa/src/app/services/sync.service.ts
+++ angular-pwa/src/app/services/sync.service.ts
@@ -1,28 +1,95 @@
 import { Injectable } from '@angular/core';
 import { OfflineService } from './offline.service';
 import { ApiService } from './api.service';
+import { interval, Subject, Observable, of, BehaviorSubject } from 'rxjs';
+import { takeUntil, take } from 'rxjs/operators';
+import Dexie from 'dexie';
 
 @Injectable({
     providedIn: 'root'
 
 })
 export class SyncService {
+    private db: any;
 
+    todos$: BehaviorSubject<any[]> = new BehaviorSubject([]);
+
+    notifier$: Subject<boolean> = new Subject<boolean>();
     constructor(private offlineService: OfflineService,
                 private apiService: ApiService) {
+        this.createIndexedDatabase();
+        this.checkConnection();
         this.initSyncTodos();
     }
 
     initSyncTodos() {
-        setTimeout(() => {
-            this.syncTodo();
-            console.log('asf');
-        }, 5000);
+        const syncInterval$ = interval(5000)
+                                .pipe(takeUntil(this.notifier$))
+                                .subscribe(() => {
+                                    this.syncTodo();
+                                });
+
     }
 
     syncTodo() {
         if (this.offlineService.isOnline) {
-            // this.getAllTodos();
+            this.apiService.getAllTodos()
+                .pipe(take(1))
+                .subscribe((values) => {
+                    this.todos$.next(values);
+                });
         }
     }
+
+    addTodo(todo: any) {
+        todo.done = false;
+        // save into the indexedDB if the connection is lost
+        if (!this.offlineService.isOnline) {
+          this.addToIndexedDb(todo);
+        } else {
+          this.apiService.addNewTodo(todo)
+            .pipe(take(1))
+            .subscribe(res => {
+              // ok saved to server
+              console.log(res);
+            });
+        }
+      }
+
+    stopInterval() {
+        this.notifier$.next(true);
+    }
+
+    sendBulkTodo(todos: any[]) {
+        return this.apiService.addNewTodo(todos);
+    }
+
+      private checkConnection() {
+        this.offlineService.connectionChanged.subscribe(online => {
+          if (online) {
+            this.sendItemsFromIndexedDb();
+          }
+        });
+      }
+
+    private createIndexedDatabase() {
+        this.db = new Dexie('TodosDatabase');
+        this.db.version(1).stores({
+          todos: 'title,done'
+        });
+        this.db.open().catch(err => console.error(err));
+      }
+
+      private async addToIndexedDb(todo: any) {
+        this.db.todos.add(todo).catch(e => {
+          console.log(e);
+        });
+      }
+
+      private async sendItemsFromIndexedDb() {
+        const allItems: any[] = await this.db.todos.toArray();
+        this.sendBulkTodo(allItems).subscribe(res => {
+          this.db.todos.clear();
+        });
+      }
 }
--- angular-pwa/src/app/services/todo.service.ts
+++ angular-pwa/src/app/services/todo.service.ts
@@ -1,73 +1,17 @@
 import { Injectable } from '@angular/core';
-import Dexie from 'dexie';
-import { OfflineService } from './offline.service';
-import { take } from 'rxjs/operators';
-import { ApiService } from './api.service';
+import { SyncService } from './sync.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
-  private db: any;
+  constructor(private syncService: SyncService) {}
 
-  constructor(private offlineService: OfflineService, private apiService: ApiService) {
-    this.createIndexedDatabase();
-    this.checkConnection();
+  addTodo(item) {
+    this.syncService.addTodo(item);
   }
 
-  addTodo(todo: any) {
-    todo.done = false;
-    // save into the indexedDB if the connection is lost
-    if (!this.offlineService.isOnline) {
-      this.addToIndexedDb(todo);
-    } else {
-      this.postTodo(todo)
-        .pipe(take(1))
-        .subscribe(res => {
-          // ok saved to server
-          console.log(res);
-        });
-    }
-  }
-
-  postTodo(item: any) {
-    return this.apiService.addNewTodo(item);
-}
-
-getAllTodos() {
-    return this.apiService.getAllTodos();
-}
-
-sendBulkTodo(todos: any[]) {
-    return this.apiService.addNewTodo(todos);
-}
-
-  private checkConnection() {
-    this.offlineService.connectionChanged.subscribe(online => {
-      if (online) {
-        this.sendItemsFromIndexedDb();
-      }
-    });
-  }
-
-  private createIndexedDatabase() {
-    this.db = new Dexie('TodosDatabase');
-    this.db.version(1).stores({
-      todos: 'title,done'
-    });
-    this.db.open().catch(err => console.error(err));
-  }
-
-  private async addToIndexedDb(todo: any) {
-    this.db.todos.add(todo).catch(e => {
-      console.log(e);
-    });
-  }
-
-  private async sendItemsFromIndexedDb() {
-    const allItems: any[] = await this.db.todos.toArray();
-    this.sendBulkTodo(allItems).subscribe(res => {
-      this.db.todos.clear();
-    });
+  getAllTodos() {
+    return this.syncService.todos$;
   }
 }
